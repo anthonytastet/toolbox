15e590f571b64c7cd1dc394d7128638dd0bb6708
anthonytastet <99203735+anthonytastet@users.noreply.github.com>
2024-01-01 01:29:35 +0100

    Update setup-environment.sh

--- sh/setup-environment.sh
+++ sh/setup-environment.sh
@@ -5,7 +5,7 @@
 apt update && \
 apt upgrade -y && \
 apt install -y xdg-utils && \
-# apt install -y firefox-esr && \
+apt install -y firefox-esr && \
 apt install -y git && \
 git config --global user.name anthony && \
 apt install -y gh && \
@@ -17,4 +17,4 @@ apt install -y python3-venv python3-pip && \
 pip3 install ansible && \
 mkdir -p /etc/ansible/hosts && \
 echo -e "[local]\n127.0.0.1   ansible_connection=local" > /etc/ansible/hosts/inventory.ini && \
-ansible-playbook provisionning-debian.yaml -i /etc/ansible/hosts/inventory.ini
+ansible-playbook /home/anthony/toolbox/yaml/provisionning-debian.yaml -i /etc/ansible/hosts/inventory.ini
